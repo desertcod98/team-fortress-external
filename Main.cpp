b1e2b04d70e1fce12f0fb8a360285cd611a60f33
des98 <dev.desertcod98@proton.me>
2023-11-07 23:29:30 +0100

    cleaned Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,29 +17,6 @@ int screenYa = GetSystemMetrics(SM_CYSCREEN);
 
 int main()
 {
-    auto memory = Memory::GetInstance();
-    auto engineDllAddress = memory->GetModuleAddress("engine.dll");
-
-    /*auto entityListBase = memory->Read<uintptr_t>(engineDllAddress + offsets::entitylist_ptr);
-
-    auto playerBase = memory->Read<uintptr_t>(entityListBase + offsets::player_ptr);
-    Entity player = Entity(memory, playerBase);
-    std::vector<Entity> entities;
-    const auto firstEntityPtr = entityListBase + offsets::first_entity_ptr;
-    Entity firstEntity = Entity(memory, memory->Read<uintptr_t>(firstEntityPtr));
-    entities.push_back(firstEntity);
-
-    for (int i = 0; i < 32; i++) {
-        const uintptr_t entityPtr = firstEntityPtr + ((i + 1) * offsets::entity_ptr_offset);
-        const auto entityBase = memory->Read<uintptr_t>(entityPtr);
-
-        if (entityBase == 0x0) break;
-
-        Entity entity = Entity(memory, entityBase);
-        entities.push_back(entity);
-    }
-    */
-
     GameData *gameData = GameData::GetInstance();
     modulesInit();
 
